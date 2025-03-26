4c6ad761f0e03eedc17be3013be179d72c8a8bd7
Soham Bhagat <bhagatsoham14@gmail.com>
2025-03-26 15:22:56 -0400

    Fixed build errors.

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,12 +1,10 @@
 import { remark } from "remark";
 import html from "remark-html";
 
+
 export default async function markdownToHtml(markdown: string) {
   const result = await remark()
-    .use(html, {
-      sanitize: false, // This allows us to add classes to the generated HTML
-      prefix: 'prose prose-slate dark:prose-invert ' // Add base classes to all content
-    })
+    .use(html) // The `remark-html` plugin does not accept options
     .process(markdown);
   return result.toString();
 }
